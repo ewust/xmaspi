970ff59d91e1d4802450e0dbff78bc8147cdec66
Eric <ewust@desky.eecs.umich.edu>
2012-12-05 03:22:48 -0500

    pretty rainbow

---
+++ rainbow.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+colors = [(255, 0, 0), \
+(255, 165, 0), \
+(255, 255, 0), \
+(0,   0,   255), \
+(0,   128, 0), \
+(64,  224, 208), \
+(238, 130, 238) ]
+
+import rgb_strand
+
+NUM_BULBS = 100
+
+strand = rgb_strand.RGBStrand(NUM_BULBS)
+for i in range(NUM_BULBS):
+    strand.set_bulb_color(colors[i][0], colors[i][1], colors[i][2])
+
+strand.set_bulb_brightness(200)
