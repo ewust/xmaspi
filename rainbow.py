c9aefc28ca966710f9e229da4fba62905c8aea3f
Eric <ewust@desky.eecs.umich.edu>
2012-12-06 01:53:06 -0500

    shifty rainbow

--- rainbow.py
+++ rainbow.py
@@ -8,6 +8,7 @@ colors = [(15, 0, 0), \
 (4,  14, 13), \
 (14, 0, 14) ]
 
+import time
 import rgb_strand
 
 NUM_BULBS = 100
@@ -15,3 +16,9 @@ NUM_BULBS = 100
 strand = rgb_strand.RGBStrand(NUM_BULBS)
 strand.set_strand_pattern(colors)
 strand.set_strand_brightness(200)
+
+while True:
+    strand.scroll_forward()
+    time.sleep(0.1)
+
+
