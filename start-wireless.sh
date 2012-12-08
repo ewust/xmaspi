9187c400d4944d037b3ed2e042fa7576cef4697b
Eric <ewust@umich.edu>
2012-12-07 19:14:49 -0500

    maybe this will reopen wireless if it dies...maybe

--- start-wireless.sh
+++ start-wireless.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-r=1
-while [[ $r -eq 1 ]];
-do 
-    iwconfig wlan1 essid xmaslights
-    sleep 1
+while [ 1 ]; do
+
     iwconfig 2>&1 | grep xmaslights > /dev/null
     r=`echo $?`
+    while [[ $r -eq 1 ]];
+    do 
+        iwconfig wlan1 essid xmaslights
+        sleep 1
+        iwconfig 2>&1 | grep xmaslights > /dev/null
+        r=`echo $?`
+    done
+
+    sleep 10
+
 done
