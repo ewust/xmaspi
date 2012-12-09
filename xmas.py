eb75b48777090265d0b43726d15e056129fbb40f
Eric <ewust@umich.edu>
2012-12-09 02:13:30 -0500

    tabs->spaces

--- xmas.py
+++ xmas.py
@@ -6,20 +6,20 @@ import twitter
 import rainbow
 
 def main():
-	l = Lock()
+    l = Lock()
     cur_running = Value('d', 0)
-	p_command_me = Process(target=command_me.func, args=(l,))
-	p_ewust = Process(target=twitter.func,args=(l,))
-	p_rainbow = Process(target=rainbow.func, args=(l,cur_running, 1))
-	p_command_me.start()
-	p_ewust.start()
-	p_rainbow.start()
+    p_command_me = Process(target=command_me.func, args=(l,))
+    p_ewust = Process(target=twitter.func,args=(l,))
+    p_rainbow = Process(target=rainbow.func, args=(l,cur_running, 1))
+    p_command_me.start()
+    p_ewust.start()
+    p_rainbow.start()
 
-	print 'All process start()\'s invoked'
+    print 'All process start()\'s invoked'
 
-	p_command_me.join()
-	p_ewust.join()
-	p_rainbow.join()
+    p_command_me.join()
+    p_ewust.join()
+    p_rainbow.join()
 
 if __name__ == "__main__":
-	main()
+    main()
