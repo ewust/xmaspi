afcdab344301d9183db437a449b3791d17bdac9c
Eric <ewust@umich.edu>
2012-12-09 22:36:26 -0500

    Test drive the priority locks

---
+++ bored.py
@@ -0,0 +1,30 @@
+
+import logger
+import time
+
+
+from multiprocessing import Lock, Value
+from remote import RemoteDriver
+import thread
+
+def start_proc(f):
+    execfile(f, globals())
+    logger.info('hmm execfile returned, weird.')
+
+def func(lock, cur_running, my_priority):
+    # let's run driver for 30 seconds:
+    RemoteDriver.set_lock(lock, cur_running, my_priority, 30, 1000)
+    
+    logger.info('Bored Sleeping for 5...')
+    time.sleep(5)
+    logger.info("Bored...running remote (don't crash on me!)")
+    thread.start_new_thread(start_proc, ('remote.py',))
+
+    end = time.time() + 30
+    while time.time() < end:
+        time.sleep(0.1)
+    
+    # should kill proc in 30 seconds or something
+    logger.info('Bored finished. Now what?')
+
+    time.sleep(100)
--- xmas.py
+++ xmas.py
@@ -4,6 +4,7 @@ from multiprocessing import Process, Lock, Value
 import command_me
 import twitter
 import rainbow
+import bored
 
 def main():
     l = Lock()
@@ -11,15 +12,18 @@ def main():
     p_command_me = Process(target=command_me.func, args=(l,))
     p_ewust = Process(target=twitter.func,args=(l,))
     p_rainbow = Process(target=rainbow.func, args=(l,cur_running, 1))
+    p_bored = Process(target=bored.func, args=(l, cur_running, 2))
     p_command_me.start()
     p_ewust.start()
     p_rainbow.start()
+    p_bored.start()
 
     print 'All process start()\'s invoked'
 
     p_command_me.join()
     p_ewust.join()
     p_rainbow.join()
+    p_bored.join()
 
 if __name__ == "__main__":
     main()
