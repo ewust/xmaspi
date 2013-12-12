039a9d5cde12a96dbb8203cbf1f53f1c4d956e9c
Pi <pi@hobocomp.com>
2013-12-12 08:47:56 +0000

    buffer in all_on

--- all_on.py
+++ all_on.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python
 
 import sys
+f = open('/dev/xmas', 'w')
 
 def send_pkt(addr, brightness, green, blue, red, strand=0):
-    f = open('/dev/xmas', 'w')
+    global f
     f.write(chr((strand << 6) | addr) + chr(brightness) + chr(green) + chr(blue) + chr(red))
-    f.close()
 
 #initialize
 
 
 brightness = int(sys.argv[1])
-b = int(sys.argv[2])
+r = int(sys.argv[2])
 g = int(sys.argv[3])
-r = int(sys.argv[4])
+b = int(sys.argv[4])
 
-strand = 0
-if len(sys.argv) > 5:
-    strand = int(sys.argv[5])
+for i in range(50):
+    send_pkt(i, 0, 0, 0, 0, 0)
+    send_pkt(i, 0, 0, 0, 0, 1)
 
 
 for i in range(50):
-    send_pkt(i, 0, 0, 0, 0, strand)
+    send_pkt(i, brightness, b, g, r, 0)
+    send_pkt(i, brightness, b, g, r, 1)
 
-
-for i in range(50):
-    send_pkt(i, brightness, b, g, r, strand)
+f.close()
